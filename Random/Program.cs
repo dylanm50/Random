ae47d96887159e2d07d2a9fcbb7b3bffc52a1fcd
PC <dylanmalcolm5@gmail.com>
2026-05-15 16:15:01 +1000

    inital

---
+++ Random/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+// Generates 'n' unique numbers within a range of 'range' in O(n) time
+
+int range = 10;
+int n = 10;
+int min = 0;
+int[] a = new int[n];
+int sect = range / n;
+Random rand = new Random();
+
+for (int i = 0; i < n; i ++)
+{
+    int max = sect * (i + 1);
+    a[i] = rand.Next(min, max);
+    min = max;
+
+    Console.WriteLine(a[i]);
+}
+
